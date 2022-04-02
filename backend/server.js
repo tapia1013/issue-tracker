8af674ae32415ca48047b0e9be12c16b9d350978
tapia1013 <manbearpig1013@gmail.com>
2022-04-02 12:30:04 -0700

    routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Issue Tracker API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes.js'))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

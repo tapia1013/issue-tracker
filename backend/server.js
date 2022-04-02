33dbb0dfb735b7031ebefd9ae75470894c61d080
tapia1013 <manbearpig1013@gmail.com>
2022-04-02 14:30:44 -0700

    create user controller logic and connect to db and userSchemas

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const dotenv = require('dotenv').config()
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware.js');
+const connectDB = require('./config/db.js');
 
 const PORT = process.env.PORT || 8000;
 
+// Connect to DataBase
+connectDB()
+
 const app = express();
+
+// Use express body parser middleware to be able to use to login,register etc.. from the forms in html AKA get data from the body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Issue Tracker API' })
-})
+});
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes.js'))
+app.use('/api/users', require('./routes/userRoutes.js'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

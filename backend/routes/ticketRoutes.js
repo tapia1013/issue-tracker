f427913fa3c25e511cc9b130a990d65bc56def86
tapia1013 <manbearpig1013@gmail.com>
2022-04-03 16:36:50 -0700

    created ticket routes

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const { protect } = require('../middleware/authMiddleware');
+
+// if we use router.route() we're allowed to add on to it
+router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+module.exports = router;

a6a69da607504fed3c1b25f18b750650d2dd01c9
tapia1013 <manbearpig1013@gmail.com>
2022-04-02 18:19:14 -0700

    home login and register ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { FaSignInAlt } from 'react-icons/fa';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+
+        <p>Please log in to get support</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              required
+              placeholder='Enter your email'
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              required
+              placeholder='Enter password'
+            />
+          </div>
+          <div className="form-group">
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 

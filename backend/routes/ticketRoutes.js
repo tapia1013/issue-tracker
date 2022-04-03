9fca2c19aea244098ceb56e0d1267d51d27ae44c
tapia1013 <manbearpig1013@gmail.com>
2022-04-03 16:48:55 -0700

    ticket routes and add to server

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const { getTickets, createTicket } = require('../controllers/ticketController');
 const { protect } = require('../middleware/authMiddleware');
 
 // if we use router.route() we're allowed to add on to it

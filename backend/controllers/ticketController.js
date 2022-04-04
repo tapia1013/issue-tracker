3126aa9b81e68405862c8d6dd3793397c91b0fdd
tapia1013 <manbearpig1013@gmail.com>
2022-04-03 17:16:32 -0700

    createticket and gettickets controller logic and routes

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,15 +7,50 @@ const Ticket = require('../models/ticketModel');
 // @route    GET /api/tickets
 // @access   Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getTickets' })
-});
+  // Get user using the ID and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found!')
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id })
+
+  res.status(200).json(tickets)
+})
 
 // @desc     Create new tickets
 // @route    POST /api/tickets
 // @access   Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: 'createTicket' })
-});
+  const { product, description } = req.body
+
+  // If no product and description sent then give error
+  if (!product || !description) {
+    res.status(400)
+    throw new Error('Please add a product and description of issue')
+  }
+
+  // Get user by ID in JWT
+  const user = await User.findById(req.user.id)
+
+  // If not user then give error
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  // Create ticket
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new'
+  })
+
+  res.status(201).json(ticket)
+})
 
 module.exports = {
   getTickets,

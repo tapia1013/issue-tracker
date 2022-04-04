5c9eddcbd19e253e350d97ebf068f0a65d7c43f1
tapia1013 <manbearpig1013@gmail.com>
2022-04-03 18:36:15 -0700

    new ticket form and state with new ticket redux in features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/new-ticket' element={<PrivateRoute />}>
-              <Route path='/new-ticket' element={NewTicket} />
+              <Route path='/new-ticket' element={<NewTicket />} />
             </Route>
           </Routes>
         </div>
